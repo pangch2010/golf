5051fb277b8b10c78a79fa9568c8784b8664b87a
pangch2010 <pangch@live.com.my>
2014-05-28 11:27:11 +0800

    checkin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-﻿var SERVER_END_POINT_API = "http://localhost:3998/";
-var URL_API = "http://localhost:3998";
-//var URL_API = "http://175.139.183.94:76/GolfAPI";
-//var SERVER_END_POINT_API = "http://175.139.183.94:76/GolfAPI/";
+﻿//var SERVER_END_POINT_API = "http://localhost:3998/";
+//var URL_API = "http://localhost:3998";
+var URL_API = "http://175.139.183.94:76/GolfAPI";
+var SERVER_END_POINT_API = "http://175.139.183.94:76/GolfAPI/";
 
 
 var defaultDate_Test = "";
@@ -41,6 +41,7 @@ function convertJsonDateTime(data) {
     );
     return dateObject;
 }
+
 $(document).on({
     ajaxStart: function () {
         showLoading();
@@ -55,6 +56,6 @@ if (localStorage.getItem("Token") != "")
     $.ajaxSetup({
     headers: {
                 "Authorization": "Bearer " + localStorage.getItem("Token")
-        }
-    });
+}
+});
 }
